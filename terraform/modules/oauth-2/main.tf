f5ebc39c485de6a184ad6364a51672e38a7d3822
Benjamin Moser <benni.moser@outlook.de>
2025-06-30 20:47:56 +0200

    OAuth-2-Proxy

---
+++ terraform/modules/oauth-2/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    exoscale = {
+      source = "exoscale/exoscale"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+# Kubernetes Namespace für den Mandanten
+resource "kubernetes_namespace" "oauth2proxy_ns" {
+  metadata {
+    name = var.namespace_name_oauth2
+  }
+}
+
+resource "helm_release" "oauth2_proxy" {
+  name       = "oauth2-proxy"
+  repository = "https://oauth2-proxy.github.io/manifests"
+  chart      = "oauth2-proxy"
+  namespace  = var.namespace_name_oauth2
+
+  values = [
+    <<-EOF
+    config:
+      clientID: "${var.oauth2_client_id}"
+      clientSecret: "${var.oauth2_client_secret}"
+      cookieSecret: "${var.oauth2_cookie_secret}"
+      provider: "github"
+      upstream=file:///dev/null
+      http-address=0.0.0.0:4180
+      redirect-url=http://fhb-key.store/oauth2/callback
+      emailDomains:
+        - "*"
+
+    ingress:
+      enabled: true
+      className: "nginx"
+      annotations: {}
+      path: /oauth2
+      pathType: Prefix
+      hosts:
+        - fhb-key.store
+      servicePort: 4180
+    EOF
+  ]
+}
