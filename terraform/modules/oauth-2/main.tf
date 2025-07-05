1522d0ea1354348c8f74870952d3f86d5bd9e1fc
Benjamin Moser <62222316+bennimoser@users.noreply.github.com>
2025-07-05 20:13:13 +0200

    Remove Scope from OAuth-2 Configuration

--- terraform/modules/oauth-2/main.tf
+++ terraform/modules/oauth-2/main.tf
@@ -36,7 +36,6 @@ resource "helm_release" "oauth2_proxy" {
         http-address = "0.0.0.0:4180"
         redirect-url = "http://fhb-key.store/oauth2/callback"
         cookie-secure = false
-        scope = "user:email"
         email-domain = "*"
       }
 
